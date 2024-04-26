ea573b34a1d2c01da6bdbbeafa45bcdd52eb224e
Tomáš Musiol <tomas.musiol@gmail.com>
2024-04-26 22:25:29 +0200

    Better layout for sheet (one column)

--- resources/views/livewire/send-for-checking.blade.php
+++ resources/views/livewire/send-for-checking.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-button x-on:click="modal='sendForChecking'" primary="true">
         <x-heroicon-o-rocket-launch />
-        Odeslat ke kontrole
+        Odeslat pracovní list ke kontrole
     </x-button>
 
     <x-modals.send-for-checking />
