c65df5351fb849588f0973be3ff107e956f12601
Tomáš Musiol <tomas.musiol@gmail.com>
2024-04-26 22:31:30 +0200

    Some layout fixes

--- resources/views/sheet/show.blade.php
+++ resources/views/sheet/show.blade.php
@@ -13,20 +13,17 @@
 
     @if ($worksheet->examples->count())
         <x-page.card>
-            <div class="pt-8">
-                <livewire:worksheet-edit :worksheet="$worksheet" />
-                <div class="grid grid-cols-1 gap-4">
-                    @foreach ($worksheet->examples as $example)
-                        <livewire:example :example="$example" key="$example->id" />
-                    @endforeach
-                </div>
-
-                @if (!$worksheet->is_finished)
-                    <div class="mt-8">
-                        <livewire:send-for-checking :worksheet="$worksheet" />
-                    </div>
-                @endif
+            <div class="grid grid-cols-1 gap-4">
+                @foreach ($worksheet->examples as $example)
+                    <livewire:example :example="$example" key="$example->id" />
+                @endforeach
             </div>
+
+            @if (!$worksheet->is_finished)
+                <div class="mt-8">
+                    <livewire:send-for-checking :worksheet="$worksheet" />
+                </div>
+            @endif
         </x-page.card>
     @endif
 @endsection
