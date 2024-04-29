b4ee1bd0bcd61726e2904e2d8dd51cd6f2d3b716
Tomáš Musiol <tomas.musiol@gmail.com>
2024-04-29 14:05:05 +0200

    typofix

--- app/Models/Sheet.php
+++ app/Models/Sheet.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Observers\sheetObserver;
+use App\Observers\SheetObserver;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
