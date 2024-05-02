416264bdd0e33a31ccddb13b72d6c6bdc4212799
Tomáš Musiol <tomas.musiol@gmail.com>
2024-05-02 11:25:11 +0200

    Footer info

--- resources/views/components/page/footer.blade.php
+++ resources/views/components/page/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="mt-8 text-sm text-neutral-400 container mx-auto px-4 2xl:px-0 2xl:max-w-7xl">
+<footer class="mt-8 text-sm text-neutral-500 container mx-auto px-4 2xl:px-0 2xl:max-w-7xl">
     <div class="space-y-1">
-        <p>KitSheet &copy; 2021 - {{ now()->year }} <a href="https://www.musiol.cz" target="_blank" class="underline hover:no-underline text-neutral-600">Tomáš Musiol</a>. Všechna práva vyhrazena.</p>
+        <p>KitSheet &copy; 2021 - {{ now()->year }} <a href="https://www.musiol.cz" target="_blank" class="underline hover:no-underline text-neutral-600">Tomáš Musiol</a> (tomas.musiol@gmail.com).</p>
         <p>Máte nějaký dotaz, připomínku nebo nápad na vylepšení? <a href="https://www.facebook.com/groups/kitsheet/" target="_blank" class="underline hover:no-underline text-neutral-600">Přidejte se do naši Facebook Skupiny</a>.</p>
     </div>
 </div>
