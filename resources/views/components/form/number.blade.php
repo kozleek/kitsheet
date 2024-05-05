ba543a4295ecd77753fe30a4edac14825f34ee6f
Tomáš Musiol <tomas.musiol@gmail.com>
2024-05-05 13:47:49 +0200

    Form elements fixes

--- resources/views/components/form/number.blade.php
+++ resources/views/components/form/number.blade.php
@@ -4,6 +4,7 @@
     <label for="{{ $name }}" class="relative block rounded-md border border-neutral-300 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
         <input
             type="number"
+            id="{{ $name }}"
             wire:model="{{ $name }}"
             @if ($change)
                 wire:change="{{ $change }}"
