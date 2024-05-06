7b6fa38a56553fda615a6b1cbb4253436eda2ca1
Tomáš Musiol <tomas.musiol@gmail.com>
2024-05-06 09:37:00 +0200

    Fixes (#26)
    * Form elements fixes
    
    * Form element focus
    
    * Better form elements
    
    ---------
    
    Signed-off-by: Tomáš Musiol <tomas.musiol@gmail.com>

--- resources/views/components/form/number.blade.php
+++ resources/views/components/form/number.blade.php
@@ -22,7 +22,7 @@
             <span class="px-2 flex gap-2 items-center pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
                 {{ $label }}
                 @if ($required)
-                    <span class="block bg-red-300 border border-red-400 w-1 h-1 rounded-full"></span>
+                    <span class="block bg-red-300 border border-red-400 size-1.5 relative -top-1 rounded-full"></span>
                 @endif
             </span>
         @endif
--- resources/views/livewire/kit-config.blade.php
+++ resources/views/livewire/kit-config.blade.php
@@ -1,18 +1,18 @@
 <x-page.card>
     <div class="space-y-4">
-        <x-form.section label="Nastavení sady">
+        <x-form.section label="Základní informace">
             <div class="grid grid-cols-1 gap-4 md:grid-cols-2 mb-8">
-                <x-form.text label="Název sady" name="title" focus="true" />
-                <x-form.text label="Stručný popis sady" name="description" />
+                <x-form.text label="Název" name="title" focus="true" />
+                <x-form.text label="Stručný popis" name="description" />
             </div>
 
             <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-                <x-form.number label="Počet pracovních listů / Počet žáků" name="countSheets" min="1" max="50" required="true" />
+                <x-form.number label="Počet pracovních listů" name="countSheets" min="1" max="50" required="true" />
                 <x-form.number label="Počet příkladů v pracovním listu" name="countExamples" min="1" max="50" required="true" />
             </div>
         </x-form.section>
 
-        <x-form.section label="Nastavení čísel">
+        <x-form.section label="Čísla">
             <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
                 <x-form.number label="Minimum" name="rangeMin" required="true" />
                 <x-form.number label="Maximum" name="rangeMax" required="true" />
@@ -30,7 +30,7 @@
             </div>
         </x-form.section>
 
-        <x-form.section label="Nastavení příkladů">
+        <x-form.section label="Nastavení">
             <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
                 <x-form.checkbox label="Pouze kladné výsledky" description="Negenerují se příklady se záporným výsledkem. Např. při odčítání" name="settingsExamplesOnlyPositive" />
                 <x-form.checkbox label="Priority operátorů" description="Do příkladů se přidají závorky pro dvojce čísel. Např.: (1 + 2) + (3 + 4) + 5 = ?" name="settingsExamplesWithParentheses" />
