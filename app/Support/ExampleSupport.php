7b64c4e1345045cb632dcd4270e87f5e5e2d7181
Tomáš Musiol <tomas.musiol@gmail.com>
2024-05-10 09:22:55 +0200

    Specifications (#29)
    * Better example data (specification as JSON default)
    
    * better addParentheses (dirty but function :)

--- app/Support/ExampleSupport.php
+++ app/Support/ExampleSupport.php
@@ -28,61 +28,95 @@ class ExampleSupport
         $range['min'] = $isDivideAllowed && $range['min'] == 0 ? 1 : $range['min'];
 
         do {
-            $numbers  = [];
-            $operations = [];
-
+            $specification = [];
             $maxNumbers = $countNumbers - 1;
-            $maxOperations = $maxNumbers - 1;
 
-            $specification = '';
-
-            // generate a random operation
-            for ($j = 0; $j <= $maxOperations; $j++) {
-                // generate a random operation
-                $operation = RandomSupport::getRandomSign($rangeOperations);
-                // add the operation to the array
-                $operations[] = $operation['sign'];
-            }
-
-            // generate random numbers
+            // every number is followed by an operation, except the last number
             for ($i = 0; $i <= $maxNumbers; $i++) {
                 // generate a random number
-                // if isDivideAllowed is true, disable 0 as a number
                 do {
                     $number = RandomSupport::getRandomNumber($range, $level);
+                    // if isDivideAllowed is true, repeat the process until the number is not 0
                 } while ($isDivideAllowed && $number == 0);
 
                 // add the number to the array
-                $numbers[] = $number;
+                $specification[] = $number;
+
+                // if not the last number, add a random operation
+                if ($i < $maxNumbers) {
+                    $specification[] = RandomSupport::getRandomSign($rangeOperations)['sign'];
+                }
             }
 
-            // create a specification
-            for ($i = 0; $i <= $maxNumbers; $i++) {
-                // create a raw specification for calculation
-                $specification .= $numbers[$i] . ($i < $maxNumbers ? $operations[$i] : '');
-            }
-
-            // add parentheses to the expression
+            // if withParentheses is true, add parentheses
             if ($withParentheses) {
-                $specification = self::addParentheses($specification, count($operations));
+                $specification = self::addParentheses($specification);
             }
 
             // get the result of the specification
-            // eval() is used to calculate the result of the specification
-            $result = $specification != '' ? eval('return ' . $specification . ';') : '';
+            $result = self::getResult($specification, $range['decimals']);
 
-            // if result is float, fix the number of decimals
-            if (is_float($result)) {
-                $result = number_format($result, $range['decimals'], ',', '');
-            }
+            // if onlyPositiveValues is true, repeat the process until the result is positive
         } while ($onlyPositiveValues && $result < 0);
 
         return [
             'specification' => $specification,
-            'json' => self::getSpecificationJSON($specification),
-            'formatted' => self::getSpecificationFormatted($specification),
+            'specification_formatted' => self::getSpecificationFormatted($specification),
             'result' => $result,
         ];
+    }
+
+    /**
+     * Add parentheses to the expression
+     * This method add parentheses to the expression.
+     */
+
+    private static function addParentheses($specification): array
+    {
+        $count = count($specification);
+        $specificationWithParentheses = $specification;
+
+        // two numbers and one operation
+        if ($count == 3) {
+            return $specification;
+        } else {
+            // three numbers and two operations
+            if ($count == 5) {
+                $specificationWithParentheses = ['(', $specification[0], $specification[1], $specification[2], ')', $specification[3], $specification[4]];
+            }
+            // four numbers and three operations
+            if ($count == 7) {
+                $specificationWithParentheses = ['(', $specification[0], $specification[1], $specification[2], ')', $specification[3], '(', $specification[4], $specification[5], $specification[6], ')'];
+            }
+            // five numbers and four operations
+            if ($count == 9) {
+                $specificationWithParentheses = ['(', $specification[0], $specification[1], $specification[2], ')', $specification[3], '(', $specification[4], $specification[5], $specification[6], ')', $specification[7], $specification[8]];
+            }
+
+            return $specificationWithParentheses;
+        }
+    }
+
+
+    /**
+     * Get result of the specification
+     * This method calculates the result of the specification
+     */
+
+    private static function getResult($specification, $decimals = 0): string
+    {
+        $specificationForEvaluation = implode($specification);
+
+        // get the result of the specification
+        // eval() is used to calculate the result of the specification
+        $result = $specification ? eval('return ' . $specificationForEvaluation . ';') : '';
+
+        // if result is float, fix the number of decimals
+        if (is_float($result)) {
+            $result = number_format($result, $decimals, ',', '');
+        }
+
+        return $result;
     }
 
     /**
@@ -96,70 +130,15 @@ class ExampleSupport
 
     private static function getSpecificationFormatted($specification): string
     {
+        $specificationString = implode($specification);
+
         // add space before and after the operators
-        $format = preg_replace('/([+\-\/*^])/', ' $1 ', $specification);
+        $format = preg_replace('/([+\-\/*^])/', ' $1 ', $specificationString);
         // replace multiplication and division operators with their symbols
         $format = Str::replace(['*', '/'], ['×', ':'], $format);
         // replace decimal point with comma
         $format = Str::replace('.', ',', $format);
 
         return $format;
-    }
-
-    /**
-     * Get specification JSON
-     * This method converts the specification to a JSON array
-     */
-
-    private static function getSpecificationJSON($specification): array
-    {
-        $json = [];
-
-        // find all numbers and operators in the specification
-        preg_match_all('/\d+|[\+\-\*\/\(\)]/', $specification, $matches);
-
-        // add each match to the array
-        foreach ($matches[0] as $match) {
-            $json[] = $match;
-        }
-
-        return $json;
-    }
-
-    /**
-     * Add parentheses to the expression
-     * This method add parentheses to the expression. In BETA only for first two operands!
-     * If the number of operands is 1 (only single operation), no parentheses need to be added
-     */
-
-    private static function addParentheses($expression, $operationsCount): string
-    {
-        // Split the string into individual operands and operators
-        $operands = preg_split('/\s*([-+*\/])\s*/', $expression, -1, PREG_SPLIT_DELIM_CAPTURE);
-        $operandsCount = count($operands);
-
-        // If the number of operands is 1 (only single operation), no parentheses need to be added
-        if ($operandsCount < 3) {
-            return $expression;
-        }
-
-        // Initialize the resulting expression with the first two operands and the operator
-        if ($operandsCount >= 3) {
-            $expressionWithParentheses = '(' . $operands[0] . $operands[1] . $operands[2] . ')';
-            $remainingStart = 3;
-        }
-
-        if ($operandsCount >= 6) {
-            $expressionWithParentheses = '(' . $operands[0] . $operands[1] . $operands[2] . ')' . $operands[3] . '(' . $operands[4] . $operands[5] . $operands[6] . ')';
-            $remainingStart = 7;
-        }
-
-        // Iterate over the remaining operands and operators
-        for ($i = $remainingStart; $i < $operandsCount; $i += 2) {
-            // Add the next operator and operand to the resulting expression
-            $expressionWithParentheses .= $operands[$i] . $operands[$i + 1];
-        }
-
-        return $expressionWithParentheses;
     }
 }
