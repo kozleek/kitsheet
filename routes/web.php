f87396bb9dadc77feff640e5ae5a85b5bcfbd60d
Tomáš Musiol <tomas.musiol@gmail.com>
2024-05-15 21:25:28 +0200

    Routes update

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,10 @@ use App\Http\Controllers\KitController;
 use App\Http\Controllers\PrintController;
 use App\Http\Controllers\SheetController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', [KitController::class, 'create'])->name('kit.create');
 Route::get('/kit/{kit}', [KitController::class, 'show'])->name('kit.show');
 Route::get('/kit/{kit}/edit', [KitController::class, 'edit'])->name('kit.edit');
+Route::get('/kit/{kit}/print', PrintController::class)->name('kit.print');
 Route::delete('/kit/{kit}', [KitController::class, 'destroy'])->name('kit.destroy');
 
-Route::get('/kit/{kit}/print', PrintController::class)->name('kit.print');
 Route::get('/sheet/{sheet}', [SheetController::class, 'show'])->name('sheet.show');
