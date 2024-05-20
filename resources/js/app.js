0b3b4065c77499d4e3b8b09703a828fbda9f39b1
Tomáš Musiol <tomas.musiol@gmail.com>
2024-05-20 10:35:04 +0200

    notifiacation position

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,16 +7,16 @@ import toastr from 'toastr';
 import 'toastr/build/toastr.min.css';
 
 toastr.options = {
-    "closeButton": true,
+    "closeButton": false,
     "debug": false,
     "newestOnTop": true,
     "progressBar": true,
-    "positionClass": "toast-bottom-right",
+    "positionClass": "toast-top-right",
     "preventDuplicates": false,
     "onclick": null,
     "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "5000",
+    "hideDuration": "300",
+    "timeOut": "2000",
     "extendedTimeOut": "1000",
     "showEasing": "swing",
     "hideEasing": "linear",
