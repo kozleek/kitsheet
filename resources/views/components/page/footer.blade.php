b2868c296d76e9bdb8abbca19706098b0f5151c9
Tomáš Musiol <tomas.musiol@gmail.com>
2024-05-27 15:21:00 +0200

    Footer links

--- resources/views/components/page/footer.blade.php
+++ resources/views/components/page/footer.blade.php
@@ -2,7 +2,8 @@
     <div class="flex flex-col md:flex-row gap-4 md:justify-between md:items-center">
         <div class="space-y-1 text-xs">
             <p>KitSheet &copy; 2021 - {{ now()->year }} <a href="https://www.musiol.cz" target="_blank" class="hover:underline text-neutral-700">Tomáš Musiol</a> (tomas.musiol@gmail.com).</p>
-            <p>Chcete dostávat novinky o nových funkcích a aktualizacích? <a href="https://nasetrida.substack.com/" target="_blank" class="hover:underline text-neutral-700">Přihlaste se k odběru novinek</a>.</p>
+            <p>Chcete dostávat novinky o nových funkcích a aktualizacích? <a href="https://nasetrida.substack.com/" target="_blank" class="underline text-neutral-700">Přihlaste se k odběru novinek e-mailem</a>.</p>
+            <p>Budete-li mít jakýkoliv dotaz, nápad na vylepšení nebo narazíte-li na chybu, napište do naši <a href="https://www.facebook.com/groups/kitsheet" target="_blank" class="underline text-neutral-700">FB skupiny</a>.</p>
         </div>
         @if (Route::is('feedback.*') === false)
             <div>
