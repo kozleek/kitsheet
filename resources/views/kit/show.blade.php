b3f50f6c22b5f29ed7f14b32d4660c0c5bd3e130
Tomáš Musiol <tomas.musiol@gmail.com>
2024-05-27 14:52:58 +0200

    QR codes for sheets

--- resources/views/kit/show.blade.php
+++ resources/views/kit/show.blade.php
@@ -23,6 +23,7 @@
                     <x-button icon="heroicon-o-adjustments-horizontal" tooltip="Upravit sadu" href="{{ route('kit.edit', ['kit' => $kit]) }}" />
                 @endif
 
+                <x-button icon="heroicon-o-qr-code" tooltip="QR kódy" x-on:click="printThis('{{ route('kit.qr', ['kit' => $kit]) }}');" />
                 <x-button icon="heroicon-o-link" tooltip="Zkopírovat odkaz" data-clipboard-text="{{ Request::url() }}" />
                 <x-button icon="heroicon-o-printer" tooltip="Tisková verze" x-on:click="printThis('{{ route('kit.print', ['kit' => $kit]) }}');" />
                 <x-button icon="heroicon-o-trash" danger="true" tooltip="Smazat sadu" x-on:click="modal='modal-kit-destroy'" />
