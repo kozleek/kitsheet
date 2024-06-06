296aa4a7b2e907cf261ca3f2fd633691c45d8623
Tomáš Musiol <tomas.musiol@gmail.com>
2024-06-06 08:42:27 +0200

    Print color fix

--- resources/views/components/info/kit.blade.php
+++ resources/views/components/info/kit.blade.php
@@ -1,7 +1,7 @@
 @props(['kit'])
 
 @if ($kit)
-    <div class="mt-8 flex flex-row sm:flex-wrap space-x-3 sm:space-x-6 text-slate-600 print:text-black">
+    <div class="mt-8 flex flex-row sm:flex-wrap space-x-3 sm:space-x-6 text-slate-600 print:text-neutral-500">
         <x-info.sheets :kit="$kit" />
         <x-info.examples :kit="$kit" />
         <x-info.numbers :kit="$kit" />
--- resources/views/components/info/operations.blade.php
+++ resources/views/components/info/operations.blade.php
@@ -3,16 +3,16 @@
 @if ($kit)
     <div class="flex items-center text-xs gap-2">
         @if (in_array('add', $kit->range_operations))
-            <span class="flex items-center justify-center rounded-md border border-slate-600 print:border-black bg-none size-5 cursor-default" data-tippy-content="Sčítání">+</span>
+            <span class="flex items-center justify-center rounded-md border border-slate-600 print:border-neutral-500 bg-none size-5 cursor-default" data-tippy-content="Sčítání">+</span>
         @endif
         @if (in_array('subtract', $kit->range_operations))
-            <span class="flex items-center justify-center rounded-md border border-slate-600 print:border-black bg-none size-5 cursor-default" data-tippy-content="Odčítání">-</span>
+            <span class="flex items-center justify-center rounded-md border border-slate-600 print:border-neutral-500 bg-none size-5 cursor-default" data-tippy-content="Odčítání">-</span>
         @endif
         @if (in_array('multiply', $kit->range_operations))
-            <span class="flex items-center justify-center rounded-md border border-slate-600 print:border-black bg-none size-5 cursor-default" data-tippy-content="Násobení">×</span>
+            <span class="flex items-center justify-center rounded-md border border-slate-600 print:border-neutral-500 bg-none size-5 cursor-default" data-tippy-content="Násobení">×</span>
         @endif
         @if (in_array('divide', $kit->range_operations))
-            <span class="flex items-center justify-center rounded-md border border-slate-600 print:border-black bg-none size-5 cursor-default" data-tippy-content="Dělení">:</span>
+            <span class="flex items-center justify-center rounded-md border border-slate-600 print:border-neutral-500 bg-none size-5 cursor-default" data-tippy-content="Dělení">:</span>
         @endif
     </div>
 @endif
