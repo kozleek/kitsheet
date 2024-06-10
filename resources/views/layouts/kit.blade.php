f577191fe3de131684d108607f70ac2e456d2983
Tomáš Musiol <tomas.musiol@gmail.com>
2024-06-10 16:34:22 +0200

    Theme color

--- resources/views/layouts/kit.blade.php
+++ resources/views/layouts/kit.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <meta name="theme-color" content="#6466F1"/>
+    <meta name="theme-color" content="#444E5D"/>
 
     <!-- HTML Meta Tags -->
     <title>{{ $pageTitle }} </title>
--- resources/views/layouts/sheet.blade.php
+++ resources/views/layouts/sheet.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="robots" content="noindex, nofollow" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <meta name="theme-color" content="#6466F1"/>
+    <meta name="theme-color" content="#444E5D"/>
 
     <!-- HTML Meta Tags -->
     <title>{{ $pageTitle }} </title>
