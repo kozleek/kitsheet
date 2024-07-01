c8feaff529d8218c79764fa3fef0671d9b2a4d64
Tomáš Musiol <tomas.musiol@gmail.com>
2024-07-01 23:38:18 +0200

    Footer author strings

--- resources/views/components/page/footer.blade.php
+++ resources/views/components/page/footer.blade.php
@@ -1,6 +1,9 @@
 <footer class="mt-8 pb-8 text-sm text-neutral-400 content-container">
     <div class="space-y-1 text-xs">
-        <p>{{ config('app.name') }} &copy; {{ config('kitsheet.year')}} - {{ now()->year }} <a href="https://www.musiol.cz" target="_blank" class="hover:underline text-neutral-100">Tomáš Musiol</a> (tomas.musiol@gmail.com).</p>
+        <p>
+            {{ config('app.name') }} &copy; {{ config('kitsheet.year')}} - {{ now()->year }}
+            <a href="{{ config('kitsheet.author_url') }}" target="_blank" class="hover:underline text-neutral-100">{{ config('kitsheet.author') }}</a> ({{ config('kitsheet.author_email') }}).
+        </p>
         <p>{!! __('footer.text_1') !!}</p>
         <p>{!! __('footer.text_2') !!}</p>
     </div>
