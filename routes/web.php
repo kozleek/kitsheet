353f710dfffadae299f08bf2e8b11e29b6b54120
Tomáš Musiol <tomas.musiol@gmail.com>
2024-07-02 08:08:40 +0200

    WIP: Routes for langs

--- routes/web.php
+++ routes/web.php
@@ -6,24 +6,20 @@ use App\Http\Controllers\KitController;
 use App\Http\Controllers\SheetController;
 use App\Http\Controllers\ReportController;
 
-Route::middleware([SetLanguage::class])->group(['prefix' => '{lang}', 'where' => ['lang' => 'en|cs']], function () {
+Route::get('/', [KitController::class, 'create'])->name('kit.create');
+Route::post('/kit/store', [KitController::class, 'store'])->name('kit.store');
+Route::get('/kit/{kit}/edit', [KitController::class, 'edit'])->name('kit.edit');
+Route::patch('/kit/{kit}/update', [KitController::class, 'update'])->name('kit.update');
+Route::get('/kit/{kit}', [KitController::class, 'show'])->name('kit.show');
+Route::delete('/kit/{kit}', [KitController::class, 'destroy'])->name('kit.destroy');
+Route::get('/kit/{kit}/print', [KitController::class, 'print'])->name('kit.print');
+Route::get('/kit/{kit}/qr', [KitController::class, 'qr'])->name('kit.qr');
+Route::get('/kit/{kit}/excel', [KitController::class, 'excel'])->name('kit.excel');
 
-    Route::get('/', [KitController::class, 'create'])->name('kit.create');
-    Route::post('/kit/store', [KitController::class, 'store'])->name('kit.store');
-    Route::get('/kit/{kit}/edit', [KitController::class, 'edit'])->name('kit.edit');
-    Route::patch('/kit/{kit}/update', [KitController::class, 'update'])->name('kit.update');
-    Route::get('/kit/{kit}', [KitController::class, 'show'])->name('kit.show');
-    Route::delete('/kit/{kit}', [KitController::class, 'destroy'])->name('kit.destroy');
-    Route::get('/kit/{kit}/print', [KitController::class, 'print'])->name('kit.print');
-    Route::get('/kit/{kit}/qr', [KitController::class, 'qr'])->name('kit.qr');
-    Route::get('/kit/{kit}/excel', [KitController::class, 'excel'])->name('kit.excel');
+Route::get('/sheet/{sheet}', [SheetController::class, 'show'])->name('sheet.show');
+Route::get('/s/{fingerprint}', [SheetController::class, 'showByFingerprint'])->name('sheet.fingerprint');
+Route::post('/sheet/{sheet}/check', [SheetController::class, 'check'])->name('sheet.check');
 
-    Route::get('/sheet/{sheet}', [SheetController::class, 'show'])->name('sheet.show');
-    Route::get('/s/{fingerprint}', [SheetController::class, 'showByFingerprint'])->name('sheet.fingerprint');
-    Route::post('/sheet/{sheet}/check', [SheetController::class, 'check'])->name('sheet.check');
-
-    Route::get('/report', [ReportController::class, 'create'])->name('report.create');
-    Route::post('/report/store', [ReportController::class, 'store'])->name('report.store');
-    Route::get('/report/{id}/thank-you', [ReportController::class, 'thankYou'])->name('report.thankYou');
-
-});
+Route::get('/report', [ReportController::class, 'create'])->name('report.create');
+Route::post('/report/store', [ReportController::class, 'store'])->name('report.store');
+Route::get('/report/{id}/thank-you', [ReportController::class, 'thankYou'])->name('report.thankYou');
