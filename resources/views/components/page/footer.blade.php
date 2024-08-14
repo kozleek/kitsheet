33810d0bc8c9de59d0b08f3f390bcc447e3ba4c0
Tomáš Musiol <tomas.musiol@gmail.com>
2024-08-14 10:56:06 +0200

    github link

--- resources/views/components/page/footer.blade.php
+++ resources/views/components/page/footer.blade.php
@@ -7,7 +7,14 @@
         <p>{!! __('footer.text_1') !!}</p>
         <p>{!! __('footer.text_2') !!}</p>
     </div>
-    <div class="mt-4">
+    <div class="mt-4 flex gap-4">
+        <a href="https://github.com/kozleek/kitsheet" target="_blank" class="button button-small bg-slate-800 text-gray-300">
+            <x-heroicon-o-code-bracket class="h-4 w-4 text-rose-500" />
+            <span class="text-xs">
+                GitHub
+            </span>
+        </a>
+
         @if (Route::is('report.*') === false)
             <a href="{{ route('report.create') }}" target="_blank" class="button button-small bg-slate-800 text-gray-300">
                 <x-heroicon-o-bug-ant class="h-4 w-4 text-rose-500" />
